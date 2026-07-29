e5d2f69a192fe601b0fdf50361df27d07639d80e
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-30 08:48:04 +0900

    因果効果推定モデルを公開

--- src/cauchan/models/__init__.py
+++ src/cauchan/models/__init__.py
@@ -1,5 +1,11 @@
 """因果探索・推論モデル。"""
 
+from .inference import CausalEffectEstimator, InferenceMethodName
 from .model import CausalDiscovery, CausalInference
 
-__all__ = ["CausalDiscovery", "CausalInference"]
+__all__ = [
+    "CausalDiscovery",
+    "CausalInference",
+    "CausalEffectEstimator",
+    "InferenceMethodName",
+]
