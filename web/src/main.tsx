830d2f0cc586c54f9b3e451142fcc6ca95635009
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-29 21:18:58 +0900

    因果構造ワークフロー用スタイルを読み込む

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "@xyflow/react/dist/style.css";
 import App from "./App";
 import "./styles.css";
+import "./structure-workflow.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
