05e0fc9273ef7f98032f9f268beda266a70c53a8
tanakakao <51264417+tanakakao@users.noreply.github.com>
2026-07-30 07:03:53 +0900

    グラフ操作スタイルを読み込む

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import "@xyflow/react/dist/style.css";
 import App from "./App";
 import "./styles.css";
 import "./structure-workflow.css";
+import "./graph-interactions.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
